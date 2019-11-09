05b8a0c8ee5dee3b9a9a81944588e7fc33c7c53f
elisim <elisimhayev@gmail.com>
2019-11-09 17:04:42 +0200

    added decay epsilon algo

--- q_learning.py
+++ q_learning.py
@@ -9,7 +9,8 @@ def q_learning(env,
                alpha=0.1,
                gamma=0.6,
                epsilon=1,
-               epsilon_decay=0.1,
+               min_epsilon=0.1,
+               epsilon_decay=0.999,
                ):
     """
     :param env: Open AI env
@@ -18,28 +19,28 @@ def q_learning(env,
     :param alpha: learning rate 𝛼
     :param gamma: discount factor 𝛾,
     :param epsilon: initial epsilon
+    :param min_epsilon: min epsilon rate (end of decaying)
     :param epsilon_decay: decay rate for decaying epsilon-greedy probability
-    :return: Q-table for each state-action pair
+    :return: Q-table for each state-action pair, and info dict for debugging & plotting
     """
 
-    # for ploting
     rewards = []  # todo: reward per episode
 
     # initialize the Q-table for each state-action pair
     q_table = np.zeros([env.observation_space.n, env.action_space.n])
-
+    
     for i in tqdm(range(1, episodes+1)):
         done = False  # indicating if the returned state is a terminal state
-        curr_steps = 0
-
+        curr_steps = 0  # init current steps for this episode
         state = env.reset()  # get initial state s
-
+        epsilon = max(min_epsilon, epsilon*(epsilon_decay ** i))  # decaying epsilon-greedy probability
+        
         while not done and curr_steps < max_steps_per_episode:
             if random.uniform(0, 1) < epsilon:
                 action = env.action_space.sample() # Explore action space
             else:
                 action = np.argmax(q_table[state]) # Exploit learned values
-
+                
             next_state, reward, done, _ = env.step(action)
             if done:
                 target = reward
@@ -53,7 +54,9 @@ def q_learning(env,
             state = next_state
             curr_steps += 1
 
-    return q_table
+            
+    info = {"rewards": rewards, "q_table_500_steps": None, "q_table_2000_steps": None}
+    return q_table, info
 
 
 def deep_q_learning():
